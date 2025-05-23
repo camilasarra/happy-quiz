aa9717d0ad79b27123f986f42c424c0ae2699f00
Camila Sarra <camilasarra@gmail.com>
2025-05-23 14:01:43 -0300

    images

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,29 @@ import './App.css'
 function App() {
   const [current, setCurrent] = useState(0)
   const [showResult, setShowResult] = useState(false)
+  const [selectedBreed, setSelectedBreed] = useState(null)
+
+  const breeds = [
+    {
+      name: 'Golden Retriever 🐾',
+      img: './public/golden.png',
+      reason:
+        'Fluffy serotonin factory. Great with people, low drama, pro cuddler.',
+    },
+    {
+      name: 'Poodle 🧠',
+      img: './public/poodle.png',
+      reason:
+        'Comes in 3 sizes. Smarter than your ex. Will run your household with grace.',
+    },
+  ]
 
   const handleAnswer = () => {
     if (current + 1 < questions.length) {
       setCurrent(current + 1)
     } else {
+      const random = Math.floor(Math.random() * breeds.length)
+      setSelectedBreed(breeds[random])
       setShowResult(true)
     }
   }
@@ -17,20 +35,22 @@ function App() {
   const restartQuiz = () => {
     setCurrent(0)
     setShowResult(false)
+    setSelectedBreed(null)
   }
 
   return (
     <div className="quiz-container">
-      {showResult ? (
+      {showResult && selectedBreed ? (
         <div className="result">
           <h2>Science has spoken.</h2>
           <p>You clearly need a dog. Like, now.</p>
-          <img src="/dog.jpg" alt="A happy dog" className="dog-img" />
-          <h3>Suggested breed: Golden Retriever 🐾</h3>
-          <p>
-            Why? Because they are fluffy serotonin factories. Great with people,
-            low drama, pro cuddler.
-          </p>
+          <img
+            src={selectedBreed.img}
+            alt={selectedBreed.name}
+            className="dog-img"
+          />
+          <h3>Suggested breed: {selectedBreed.name}</h3>
+          <p>{selectedBreed.reason}</p>
           <button onClick={restartQuiz}>Take it again</button>
         </div>
       ) : (
